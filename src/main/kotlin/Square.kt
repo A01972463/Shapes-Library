401feadd37dc4c70d1cce843d04eb268374b2e97
Gustavo <a01972463@usu.edu>
2024-07-01 14:50:35 -0600

    Update Square.kt
    First draft of Square class

--- src/main/kotlin/Square.kt
+++ src/main/kotlin/Square.kt
@@ -1,4 +1,19 @@
 package org.example
 
-class Square {
+class Square(var Point: Point, var sideLength: Double) {
+    fun getPoints(): Any {
+        val x1 = Point.x
+        val y1 = Point.y
+
+        return listOf(Point, Point(x1 + sideLength, y1), Point(x1 + sideLength, y1 + sideLength), Point(x1, y1 + sideLength))
+    }
+
+    fun getArea(): String {
+        return String.format("%.3f", sideLength * sideLength)
+    }
+
+    fun moveSquare(newPoint: Point, newSideLength: Double) {
+        Point = newPoint
+        sideLength = newSideLength
+    }
 }
