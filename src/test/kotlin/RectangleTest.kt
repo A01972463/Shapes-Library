03b36dce477012521fb647405e40704bb6f04e3d
Gustavo <a01972463@usu.edu>
2024-07-01 14:22:31 -0600

    Drafting Square/Rectangle Tests
    Created first draft of Rectangle and Square unit tests.

--- src/test/kotlin/RectangleTest.kt
+++ src/test/kotlin/RectangleTest.kt
@@ -1,33 +1,68 @@
-//import org.example.Point
-//import org.example.Rectangle
-//import kotlin.test.Test
-//import kotlin.test.assertEquals
-//
-//class RectangleTest {
-//    var PointA = Point(0.0, 0.0)
-//    var PointB = Point(2.0, -3.5)
-//    var Rectangle = Rectangle(PointA, PointB)
-//
-//    @Test
-//    fun testRectangleConstruction() {
-//        assertEquals(0.0, Rectangle.pointA.x)
-//        assertEquals(0.0, Rectangle.pointA.y)
-//        assertEquals(2.0, Rectangle.pointB.x)
-//        assertEquals(-3.5, Rectangle.pointB.x)
-//    }
-//
-//    @Test
-//    fun testRectangleGetPoints() {
-//
-//    }
-//
-//    @Test
-//    fun testRectangleGetArea() {
-//
-//    }
-//
-//    @Test
-//    fun testMoveRectangle() {
-//
-//    }
-//}
+import org.example.Point
+import org.example.Rectangle
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class RectangleTest {
+    var PointA = Point(0.0, 0.0)
+    var PointB = Point(2.0, -3.5)
+    var Rectangle = Rectangle(PointA, PointB)
+
+    @Test
+    fun testRectangleConstruction() {
+        assertEquals(0.0, Rectangle.pointA.x)
+        assertEquals(0.0, Rectangle.pointA.y)
+        assertEquals(2.0, Rectangle.pointB.x)
+        assertEquals(-3.5, Rectangle.pointB.x)
+    }
+
+    @Test
+    fun testRectangleGetPoints() {
+        val points = listOf(Rectangle.getPoints())
+
+        assertEquals(0.0, points[0].x)
+        assertEquals(0.0, points[0].y)
+        assertEquals(2.0, points[1].x)
+        assertEquals(0.0, points[1].y)
+        assertEquals(2.0, points[2].x)
+        assertEquals(-3.5, points[2].y)
+        assertEquals(0.0, points[3].x)
+        assertEquals(-3.5, points[3].y)
+    }
+
+    @Test
+    fun testRectangleGetArea() {
+        assertEquals("7.00", Rectangle.getArea())
+    }
+
+    @Test
+    fun testMoveRectangle() {
+//        Move Point
+        PointA.movePoint(-1.0, -1.0)
+        PointB.movePoint(0.0, 2.0)
+        Rectangle.moveRectangle(PointA, PointB)
+
+        assertEquals(-1.0, Rectangle.pointA.x)
+        assertEquals(-1.0, Rectangle.pointA.y)
+        assertEquals(0.0, Rectangle.pointB.x)
+        assertEquals(2.0, Rectangle.pointB.y)
+
+//        New Point
+        val newPointA = Point(0.0, 0.0)
+        val newPointB = Point(1.0, 1.0)
+        Rectangle.moveRectangle(newPointA, newPointB)
+
+        assertEquals(0.0, Rectangle.pointA.x)
+        assertEquals(0.0, Rectangle.pointA.y)
+        assertEquals(1.0, Rectangle.pointB.x)
+        assertEquals(1.0, Rectangle.pointB.y)
+
+//        Direct Point
+        Rectangle.moveRectangle(Point(-1.0, -1.0), Point(0.0, 2.0))
+
+        assertEquals(-1.0, Rectangle.pointA.x)
+        assertEquals(-1.0, Rectangle.pointA.y)
+        assertEquals(0.0, Rectangle.pointB.x)
+        assertEquals(2.0, Rectangle.pointB.y)
+    }
+}
--- src/test/kotlin/SquareTest.kt
+++ src/test/kotlin/SquareTest.kt
@@ -1,33 +1,58 @@
-//import org.example.Point
-//import org.example.Square
-//import kotlin.test.Test
-//import kotlin.test.assertEquals
-//
-//class SquareTest {
-//    var PointA = Point(0.0, 0.0)
-//    var Square = Square(PointA, 5.0)
-//
-//    @Test
-//    fun testSquareConstruction() {
-//        assertEquals(0.0, Square.pointA.x)
-//        assertEquals(0.0, Square.pointA.y)
-//        assertEquals(5.0, Square.pointB.x)
-//        assertEquals(5.0, Square.pointB.x)
-//        assertEquals(5.0, Square.sideLength)
-//    }
-//
-//    @Test
-//    fun testSquareGetPoints() {
-//
-//    }
-//
-//    @Test
-//    fun testSquareGetArea() {
-//
-//    }
-//
-//    @Test
-//    fun testMoveSquare() {
-//
-//    }
-//}
+import org.example.Point
+import org.example.Square
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SquareTest {
+    var Point = Point(0.0, 0.0)
+    var Square = Square(Point, 5.0)
+
+    @Test
+    fun testSquareConstruction() {
+        assertEquals(0.0, Square.Point.x)
+        assertEquals(0.0, Square.Point.y)
+        assertEquals(5.0, Square.sideLength)
+    }
+
+    @Test
+    fun testSquareGetPoints() {
+        val points = listOf(Square.getPoints())
+
+        assertEquals(0.0, points[0].x)
+        assertEquals(0.0, points[0].y)
+        assertEquals(5.0, points[1].x)
+        assertEquals(0.0, points[1].y)
+        assertEquals(5.0, points[2].x)
+        assertEquals(-5.0, points[2].y)
+        assertEquals(0.0, points[3].x)
+        assertEquals(-5.0, points[3].y)
+    }
+
+    @Test
+    fun testSquareGetArea() {
+        assertEquals("25.00", Square.getArea())
+    }
+
+    @Test
+    fun testMoveSquare() {
+//        Move Point
+        Point.movePoint(-1.0, -1.0)
+        Square.moveSquare(Point, 2.0)
+
+        assertEquals(-1.0, Square.Point.x)
+        assertEquals(-1.0, Square.Point.y)
+
+//        New Point
+        val newPoint = Point(0.0, 0.0)
+        Square.moveSquare(newPoint, 5.0)
+
+        assertEquals(0.0, Square.Point.x)
+        assertEquals(0.0, Square.Point.y)
+
+//        Direct Point
+        Square.moveSquare(Point(-1.0, -1.0), 2.0)
+
+        assertEquals(-1.0, Square.Point.x)
+        assertEquals(-1.0, Square.Point.y)
+    }
+}
